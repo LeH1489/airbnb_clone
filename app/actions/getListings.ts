90736b97f2683103f04a78be79e7010c43b88a60
LeH1489 <93253543+LeH1489@users.noreply.github.com>
2023-08-19 10:13:27 +0700

    3

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -48,7 +48,7 @@ export default async function getListings(params: IListingsParams) {
     }
 
     if (bathRoomCount) {
-      query.guestCount = {
+      query.bathRoomCount = {
         gte: +bathRoomCount,
       };
     }
